e4609c4e8daf37e677f09427266d6b398ba3b38e
Michael Snoyman <michael@snoyman.com>
2021-07-29 08:00:48 +0300

    Use a KeyringErrorAdapter on Linux
    Fixes the compilation issue reported in #1

--- terra-rust-wallet/src/errors.rs
+++ terra-rust-wallet/src/errors.rs
@@ -44,16 +44,31 @@ error_chain! {
 #[derive(Error, Debug)]
 pub enum TerraRustWalletError {
     #[error(transparent)]
-    KeyringError(#[from] ::keyring::KeyringError),
+    KeyringError(#[from] KeyringErrorAdapter),
     #[error(transparent)]
     SerdeJsonError(#[from] ::serde_json::Error),
 
     #[error("Terra Wallet `{key}` Key not found Error")]
     KeyNotFound {
         key: String,
-        source: keyring::KeyringError,
+        source: KeyringErrorAdapter,
     },
 
     #[error("unknown Terra-Rust Wallet error")]
     Unknown,
+}
+
+/// Workaround type to provide [Sync] on Linux.
+///
+/// On Linux, [keyring::KeyringError] does not implement `Sync` due to depending
+/// on an older version of the `dbus` crate. This prevents usage of `anyhow`. This
+/// wrapper is used to bypass that issue on Linux.
+#[derive(Error, Debug)]
+#[error(transparent)]
+pub struct KeyringErrorAdapter(anyhow::Error);
+
+impl From<keyring::KeyringError> for KeyringErrorAdapter {
+    fn from(e: keyring::KeyringError) -> Self {
+        KeyringErrorAdapter(anyhow::anyhow!("Keyring error: {:?}", e))
+    }
 }
